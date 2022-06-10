b393cfa40521e851f152eac78eabfca494ace524
Marcell Feher <sw0rdf1sh@eng.au.dk>
2022-06-10 15:39:36 +0200

    Custom tablescan for GdSegment variants disabled

--- src/lib/operators/table_scan/column_vs_value_table_scan_impl.cpp
+++ src/lib/operators/table_scan/column_vs_value_table_scan_impl.cpp
@@ -49,6 +49,7 @@ void ColumnVsValueTableScanImpl::_scan_non_reference_segment(
   if (const auto* dictionary_segment = dynamic_cast<const BaseDictionarySegment*>(&segment)) {
     _scan_dictionary_segment(*dictionary_segment, chunk_id, matches, position_filter);
   }
+  /*
   else if (const auto* gd_segment = dynamic_cast<const BaseGdSegment*>(&segment)) {
     gd_segment->segment_vs_value_table_scan(
         predicate_condition, 
@@ -56,7 +57,9 @@ void ColumnVsValueTableScanImpl::_scan_non_reference_segment(
         chunk_id, 
         matches,
         position_filter);
-  } else {
+  } 
+  */
+  else {
     _scan_generic_segment(segment, chunk_id, matches, position_filter);
   }
 }
@@ -70,8 +73,12 @@ void ColumnVsValueTableScanImpl::_scan_generic_segment(
     // ReferenceSegments are handled via position_filter
     // GdSegmentV1 handled with its own tablescan implementation
     if constexpr (!is_dictionary_segment_iterable_v<typename decltype(it)::IterableType> &&
-                  !is_reference_segment_iterable_v<typename decltype(it)::IterableType> && 
-                  !is_gd_segment_v1_iterable_v<typename decltype(it)::IterableType>) {
+                  !is_reference_segment_iterable_v<typename decltype(it)::IterableType> 
+                  /*
+                  && !is_gd_segment_v1_iterable_v<typename decltype(it)::IterableType>
+                  && !is_gd_segment_v1_fixed_iterable_v<typename decltype(it)::IterableType>
+                  */
+                  ) {
       using ColumnDataType = typename decltype(it)::ValueType;
 
       const auto typed_value = boost::get<ColumnDataType>(value);
